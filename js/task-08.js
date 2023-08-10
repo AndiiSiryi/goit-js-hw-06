3b5b55be1b2fa34708d51d11ccae2a359b1302ad
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-08-10 16:13:44 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form')
 
+formEl.addEventListener('submit', onSubmit);
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    console.log(evt.currentTarget.elements);
+
+    if (evt.currentTarget.elements.email.value === '' ||
+        evt.currentTarget.elements.password.value === '') {
+        alert('Всі поля повинні бути заповнені!')
+    } else {
+        const { email, password } = evt.currentTarget.elements;
+        const formDataObject = {
+            email: email.value,
+            password: password.value,
+        };
+        
+        console.log(formDataObject);
+        formEl.reset();
+    };
+};
