f3b9eacb71a1d80c0700ec5cbc32217eda15c870
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-08-10 16:37:03 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widgetEL = document.querySelector('.widget');
+const buttonEl = document.querySelector('.change-color');
+const currentColorEl = document.querySelector('.color'); 
+
+buttonEl.addEventListener('click', onButton);
+
+function onButton(evt) {
+  const randomColor = getRandomHexColor();
+  console.log(randomColor);
+  
+  document.body.style.backgroundColor = randomColor
+  currentColorEl.textContent = randomColor;
+}
